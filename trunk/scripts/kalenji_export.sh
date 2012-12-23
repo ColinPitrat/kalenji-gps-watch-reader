fb092b584eb243a05cf662479113bcb68b209726
colin.pitrat@gmail.com <colin.pitrat@gmail.com@ec2cdefd-334d-0cc6-ff8b-54fd0f93ec2f>
2012-12-23 09:07:00 +0000

    Add utility scripts

---
+++ trunk/scripts/kalenji_export.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+then
+	echo "Usage: $0 <GPX file>"
+	echo " GPX File: GPX file to export to the watch"
+fi
+FILE=`realpath "$1"`
+BINPATH=`realpath "$0"`
+BINDIR=`dirname "$BINPATH"`
+
+pushd $BINDIR > /dev/null
+echo $BINDIR
+echo $BINPATH
+./kalenji_reader -D GPX -i "$FILE" -f FixElevation,ReducePoints,ComputeSessionStats -o Kalenji
+popd > /dev/null
