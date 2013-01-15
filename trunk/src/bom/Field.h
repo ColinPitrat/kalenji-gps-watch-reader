5a35b745eeee98b7db1715b83d4c111ed94f432c
colin.pitrat@gmail.com <colin.pitrat@gmail.com@ec2cdefd-334d-0cc6-ff8b-54fd0f93ec2f>
2013-01-15 19:36:38 +0000

    Add fields that can hold undefined values. Few redesign for homogeneity.

---
+++ trunk/src/bom/Field.h
@@ -0,0 +1,46 @@
+#ifndef _BOM_FIELD_H
+#define _BOM_FIELD_H
+
+class TFieldUndef {};
+
+static const TFieldUndef FieldUndef;
+
+template<typename T>
+class Field
+{
+	public:
+		// Constructor from a value. If we have a value, then the field is valid.
+		Field(T value) 
+		{ 
+			_defined = true; 
+			_value = value;
+		};
+
+		// Constructor of an invalid field, using the constant defined above.
+		Field(TFieldUndef unused)
+		{
+			_defined = false;
+		};
+
+		// Implicit conversion
+		operator T () { return _value; };
+
+		// A function to be able to print a string containing the value only if field is defined
+		std::string toStream(std::string before = "", std::string after = "") 
+		{ 
+			if (_defined) 
+			{
+				std::ostringstream oss;
+				oss << before << _value << after;
+				return oss.str(); 
+			}
+			return "";
+		}
+		
+
+	private:
+		T _value;
+		bool _defined;
+};
+
+#endif
--- trunk/src/bom/Lap.h
+++ trunk/src/bom/Lap.h
@@ -8,7 +8,7 @@ class Lap
 	public:
 		Lap() : _firstPointId(0), _lastPointId(0), 
 		        _start(NULL), _end(NULL), 
-			_duration(0), _length(0), 
+			_duration(0), _distance(0), 
 			_max_speed(0), _avg_speed(0), 
 			_max_hr(0), _avg_hr(0), 
 			_calories(0), _grams(0), 
@@ -16,13 +16,13 @@ class Lap
 		{ };
 
 		Lap(uint32_t firstPointId, uint32_t lastPointId, 
-		    double duration, uint32_t length, 
+		    double duration, uint32_t distance, 
 		    double max_speed, double avg_speed, 
 		    double max_hr, double avg_hr, 
 		    uint32_t calories, uint32_t grams, 
 		    uint32_t descent, uint32_t ascent) : _firstPointId(firstPointId), _lastPointId(lastPointId), 
 		                                         _start(NULL), _end(NULL), 
-							 _duration(duration), _length(length), 
+							 _duration(duration), _distance(distance), 
 							 _max_speed(max_speed), _avg_speed(avg_speed), 
 							 _max_hr(max_hr), _avg_hr(avg_hr), 
 							 _calories(calories), _grams(grams), 
@@ -32,26 +32,28 @@ class Lap
 		void setLapNum(uint32_t lapNum)         { _lapNum = lapNum; };
 		void setStartPoint(Point *start)        { _start = start; };
 		void setEndPoint(Point *end)            { _end = end; };
+
 		void setDuration(double duration)       { _duration = duration; };
-		void setCalories(uint32_t calories)     { _calories = calories; };
-		void setLength(uint32_t length)         { _length = length; };
+		void setDistance(uint32_t distance)     { _distance = distance; };
 		void setMaxSpeed(uint32_t max_speed)    { _max_speed = max_speed; };
 		void setAvgSpeed(uint32_t avg_speed)    { _avg_speed = avg_speed; };
 		void setMaxHeartrate(uint32_t max_hr)   { _max_hr = max_hr; };
 		void setAvgHeartrate(uint32_t avg_hr)   { _avg_hr = avg_hr; };
+		void setCalories(uint32_t calories)     { _calories = calories; };
 
 		uint32_t getLapNum()       { return _lapNum; };
 		uint32_t getFirstPointId() { return _firstPointId; };
 		uint32_t getLastPointId()  { return _lastPointId; };
 		Point *getStartPoint()     { return _start; };
 		Point *getEndPoint()       { return _end; };
+
 		double getDuration()       { return _duration; };
-		uint32_t getCalories()     { return _calories; };
-		uint32_t getLength()       { return _length; };
+		uint32_t getDistance()     { return _distance; };
 		uint32_t getMaxSpeed()     { return _max_speed; };
 		uint32_t getAvgSpeed()     { return _avg_speed; };
 		uint32_t getMaxHeartrate() { return _max_hr; };
 		uint32_t getAvgHeartrate() { return _avg_hr; };
+		uint32_t getCalories()     { return _calories; };
 
 	private:
 		uint32_t _lapNum;
@@ -59,16 +61,17 @@ class Lap
 		uint32_t _lastPointId;
 		Point *_start;
 		Point *_end;
+
 		double _duration;
-		uint32_t _length;
+		uint32_t _distance;
 		double _max_speed;
 		double _avg_speed;
 		uint32_t _max_hr;
 		uint32_t _avg_hr;
 		uint32_t _calories;
 		uint32_t _grams;
-		uint32_t _descent;
 		uint32_t _ascent;
+		uint32_t _descent;
 };
 
 #endif
