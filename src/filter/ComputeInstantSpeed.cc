fb8f4e6d26e5029fc2893cdd521bc0a34b902727
colin.pitrat@gmail.com <colin.pitrat@gmail.com@ec2cdefd-334d-0cc6-ff8b-54fd0f93ec2f>
2014-06-03 19:11:48 +0000

    New filter for issue 26

---
+++ src/filter/ComputeInstantSpeed.cc
@@ -0,0 +1,30 @@
+#include "ComputeInstantSpeed.h"
+#include "../Utils.h"
+#include <cmath>
+
+namespace filter
+{
+	REGISTER_FILTER(ComputeInstantSpeed);
+
+	void ComputeInstantSpeed::filter(Session *session)
+	{
+		std::list<Point*> &points = session->getPoints();
+		Point* prevPoint = NULL;
+
+		std::list<Point*>::iterator it = points.begin();
+		double speed = 0;
+		while(true)
+		{
+			prevPoint = *it;
+			++it;
+			if(it == points.end()) break;
+
+			uint32_t distance = distanceEarth(**it, *prevPoint);
+
+			speed = (3.6 * distance) / ((*it)->getTime() - prevPoint->getTime());
+			prevPoint->setSpeed(speed);
+		}
+		// On last point, set same speed as on the previous point
+		prevPoint->setSpeed(speed);
+	}
+}
