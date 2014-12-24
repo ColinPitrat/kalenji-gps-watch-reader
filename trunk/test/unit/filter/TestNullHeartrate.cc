44b17c58ae0fa3434c3f62746892d3a349c71676
colin.pitrat@gmail.com <colin.pitrat@gmail.com@ec2cdefd-334d-0cc6-ff8b-54fd0f93ec2f>
2014-12-24 12:39:42 +0000

    New test suite

---
+++ trunk/test/unit/filter/TestNullHeartrate.cc
@@ -0,0 +1,59 @@
+#include <gtest/gtest.h>
+#include <filter/NullHeartrate.h>
+#include <bom/Session.h>
+#include <bom/Point.h>
+
+class NullHeartrateTest : public testing::Test 
+{
+	protected:
+		std::map<std::string, std::string> emptyConfiguration;
+		filter::NullHeartrate aNullHeartrateFilter;
+		Session aSession;
+};
+
+TEST_F(NullHeartrateTest, BetweenTwoPointsWithSameValue)
+{
+	Point *firstPoint, *secondPoint, *thirdPoint;
+	firstPoint = new Point(FieldUndef, FieldUndef, FieldUndef, FieldUndef, 0, 0, 100, 3);
+	aSession.addPoint(firstPoint);
+	secondPoint = new Point(FieldUndef, FieldUndef, FieldUndef, FieldUndef, 30, 0, 0, 3); 
+	aSession.addPoint(secondPoint);
+	thirdPoint = new Point(FieldUndef, FieldUndef, FieldUndef, FieldUndef, 60, 0, 100, 3); 
+	aSession.addPoint(thirdPoint);
+
+	aNullHeartrateFilter.filter(&aSession, emptyConfiguration);
+
+	ASSERT_EQ(secondPoint->getHeartRate(), 100);
+}
+
+TEST_F(NullHeartrateTest, BetweenTwoPointsWithDifferentValues)
+{
+	Point *firstPoint, *secondPoint, *thirdPoint;
+	firstPoint = new Point(FieldUndef, FieldUndef, FieldUndef, FieldUndef, 0, 0, 95, 3);
+	aSession.addPoint(firstPoint);
+	secondPoint = new Point(FieldUndef, FieldUndef, FieldUndef, FieldUndef, 30, 0, 0, 3); 
+	aSession.addPoint(secondPoint);
+	thirdPoint = new Point(FieldUndef, FieldUndef, FieldUndef, FieldUndef, 60, 0, 105, 3); 
+	aSession.addPoint(thirdPoint);
+
+	aNullHeartrateFilter.filter(&aSession, emptyConfiguration);
+
+	// Current implementation is that the last heartrate is copied until a non-null heartrate is found
+	// Interpolating could be a better idea
+	ASSERT_NE(secondPoint->getHeartRate(), 95);
+}
+
+TEST_F(NullHeartrateTest, FirstPointNotFixed)
+{
+	Point *firstPoint, *secondPoint, *thirdPoint;
+	firstPoint = new Point(FieldUndef, FieldUndef, FieldUndef, FieldUndef, 0, 0, 0, 3);
+	aSession.addPoint(firstPoint);
+	secondPoint = new Point(FieldUndef, FieldUndef, FieldUndef, FieldUndef, 30, 0, 100, 3); 
+	aSession.addPoint(secondPoint);
+	thirdPoint = new Point(FieldUndef, FieldUndef, FieldUndef, FieldUndef, 60, 0, 100, 3); 
+	aSession.addPoint(thirdPoint);
+
+	aNullHeartrateFilter.filter(&aSession, emptyConfiguration);
+
+	ASSERT_EQ(firstPoint->getHeartRate(), 0);
+}
