11fe198cb0501c270afee9bb104e07938c48b8ab
Colin Pitrat <colin.pitrat@gmail.com>
2020-08-16 12:38:24 +0100

    Move all integrated tests output to a kalenji_reader_test directory under /tmp and clean it at the beginning of the test run.

--- test/integrated/case9/run.sh
+++ test/integrated/case9/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#../../../kalenji_reader -c kalenji_readerrc -D GPX -f ComputeSessionStats -p /tmp/case9 -o OnMove710 -i Tour_etang_viry.gpx 2>&1
-../../../kalenji_reader -c kalenji_readerrc -D GPX -f ComputeSessionStats -o OnMove710 -i Tour_etang_viry.gpx -p /tmp/case9 2>&1
+#../../../kalenji_reader -c kalenji_readerrc -D GPX -f ComputeSessionStats -p /tmp/kalenji_reader_test/case9 -o OnMove710 -i Tour_etang_viry.gpx 2>&1
+../../../kalenji_reader -c kalenji_readerrc -D GPX -f ComputeSessionStats -o OnMove710 -i Tour_etang_viry.gpx -p /tmp/kalenji_reader_test/case9 2>&1
