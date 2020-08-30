91bd0deb6b28cd01aa91ec44790de223523e6d0a
Colin Pitrat <colin.pitrat@gmail.com>
2020-08-30 11:34:42 +0100

    Add a test for windows dependencies. #81

---
+++ test/integrated/test_win_deps.sh
@@ -0,0 +1,35 @@
+# The idea of this test is to ensure no new DLL dependency is added involuntarly.
+# All needed DLLs should be included in the archive.
+#
+# If the test fails, the package needs to be verified manually under windows or
+# wine to see if a DLL needs to be added to it. If so, it needs to be added to
+# the DLLS variable in makefile.
+ARCH=$1
+ACTUAL=$(mktemp)
+EXPECTED=$(mktemp)
+
+if [ "${ARCH}" == "win32" ]
+then
+  echo "Skipping test for ${ARCH}"
+  exit 0
+fi
+
+cat << EOF > ${EXPECTED}
+TODO: Fill the list of libs
+EOF
+
+echo "ldd output:"
+ldd win/kalenji_reader.exe
+
+ldd win/kalenji_reader.exe | grep mingw | sed 's/ =>.*//' | sed 's/^[ \t]*//' | sort > ${ACTUAL}
+
+echo "mingw dependencies:"
+cat "${ACTUAL}"
+
+if ! diff ${ACTUAL} ${EXPECTED}
+then
+  echo "!! dependencies test for ${ARCH} failed"
+  exit 1
+else
+  echo "The test was successful!"
+fi
